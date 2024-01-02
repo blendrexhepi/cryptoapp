4aee806023b67d423f40d8b1686474df3ddd180a
BlendRexhepi <br52443@ubt-uni.net>
2024-01-02 20:39:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import { Coin } from './components/Coin';
 
 function App() {
+ const [listOfCoins, setListOfCoins] = useState([]);
+ const [searchWord, setSearchWord] = useState("")
+
+  useEffect(() => {
+    Axios.get("https://api.coingecko.com/api/v3/search/trending").then((response) => {
+      setListOfCoins(response.data.coins);
+    })
+  }, [])
+
+  const fileteredCoins = listOfCoins.filter((coin) => {
+    return coin.item.name.toLowerCase().includes(searchWord.toLowerCase());
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='cryptoHeader'>
+        <input type='text' placeholder='Search Your Coin...' onChange={(event) => {setSearchWord(event.target.value)}} />
+      </div>
+      <div className='cryptoDisplay'>{fileteredCoins.map((coin) => {
+        return (
+           <Coin key={coin.item.id} name={coin.item.name} price={coin.item.price_btc} icon={coin.item.large} symbol={coin.item.symbol} />
+        )
+      })}</div>
     </div>
   );
 }
